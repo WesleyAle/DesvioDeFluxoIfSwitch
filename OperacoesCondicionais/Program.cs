f3983d69fb3cf841d957f23e298ae3febd818370
Wesley Alexandre <wesleygamersz@hotmail.com>
2018-04-19 11:12:06 -0300

    Declaração de variavel

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
-
+﻿
+using System;
 namespace OperacoesCondicionais
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                //Comentário in-line (em linha)
+        /*
+                 Comentário in-block (em bloco)
+         */
+         // Declaração da variavel Operação. Ela será do tipo Int (Inteira)
+         int operacao;
+
+         // Declaração das variaveis n1 e n2. Elas serão do tipo Double
+         double n1, n2;
+         
         }
     }
 }
